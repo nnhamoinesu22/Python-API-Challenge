74f34a8bdf810d50e29f238a4ccf9cacc26c2c9b
Nyarai Nhamoinesu <nyarainhamoinesu@gmail.com>
2020-01-24 15:37:13 -0600

    add file

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6d74309364ab27c0e95be79d6af38e06"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDJ5ipn_1C-60iTCjQlgU8kWZadVfcgy8g"
